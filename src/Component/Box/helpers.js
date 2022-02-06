56d162cdbcfbed3e7c699c303ecea71f8b040bfa
Tosin Mayowa <toss800@gmail.com>
2022-02-06 23:20:05 +0100

    make the parent smarter

--- src/Component/Box/helpers.js
+++ src/Component/Box/helpers.js
@@ -1,12 +1,11 @@
 function hexColor(arr,num){
 let hxColor='#';
-let newArr=[];
 for(let i=0; i<num; i++){
     let rand=Math.floor(Math.random()*arr.length);
     hxColor +=arr[rand];
 }
-newArr.push(hxColor);
-return newArr[0];
+return hxColor;
+
 }
 
 export {hexColor};
--- src/Component/BoxContainer/BoxContainer.js
+++ src/Component/BoxContainer/BoxContainer.js
@@ -1,12 +1,39 @@
 import React, {Component} from 'react';
 import Box from '../Box/Box';
 import './BoxContainer.css';
-
+import {hexColor} from '../Box/helpers';
 class BoxContainer extends Component{
+    static defaultProps={
+        ar:[1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'],
+        idx:6,
+        size:20
+    }
+    constructor(props){
+        super(props);
+        this.state={
+            box:Array.from({length:this.props.size},(x,i)=>({color:'green',isClicked:false,id:i}))
+        };
+        this.addItem=this.addItem.bind(this);
+    }
+
+    addItem(id,item){
+        const {ar,idx}=this.props;
+        let newColor;
+        do {newColor=hexColor(ar,idx);}
+     while(newColor===this.state.box.color)
+        const updatedBox=this.state.box.map(n=>{
+            if(n.id===id){
+                return {...n,isClicked:item,color:newColor}
+            }else{
+                return n;
+            }
+        });
+        this.setState({box:updatedBox})
+    }
     render(){
         return(
             <div className='BoxContainer'>
-            { Array.from({length:20}).map(n=><Box />)}
+           {this.state.box.map(n=><Box n={n} addItem={this.addItem}/>)}
             </div>
         )
     }
